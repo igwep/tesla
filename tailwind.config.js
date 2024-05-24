7c14d4d9066376e2ccbc9a7b1f8cedf738c83b54
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-05-24 12:20:05 +0100

    first cmmit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],
+  theme: {
+    extend: {
+     fontFamily:{
+      title:['Space Grotes'],
+      tes:['TESLA']
+     },
+     screens:{
+      "tablet": "2900px",
+      "smalls": "909px",
+      "another": "900px",
+      "and":"800",
+      "again":"1700px"
+    }
+    },
+  },
+  plugins: [],
+}
+
