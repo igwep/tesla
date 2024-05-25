79c484387f0f41421720b2284d4b2aa6574aaaa9
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-05-25 01:25:37 +0100

    coking

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ const linkHead = document.querySelector('.link-head');
 document.querySelectorAll('.links').forEach((link)=>{
     link.addEventListener('mouseover', () =>{
         linkHead.classList.add('links-toggle');
-       
+       document.querySelector('.example').classList.remove('hidden');
     })
     linkHead.addEventListener('mouseleave', () =>{
         linkHead.classList.remove('links-toggle')
+        document.querySelector('.example').classList.add('hidden');
     })
 })
