721c74194c416379561db99e6f9624b0b78bea1f
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-05-25 20:15:23 +0100

    cooking

--- js/app.js
+++ js/app.js
@@ -1,15 +1,42 @@
 import { menu } from "./menu.js";
 const linkHead = document.querySelector('.link-head');
 const linkMenu = document.querySelector('.links-menu');
+
+let LinkID;
+
+
+
+document.querySelectorAll('.links').forEach((link)=>{
+    link.addEventListener('mouseover', (e) =>{
+        linkHead.classList.add('links-toggle');
+       document.querySelector('.example').classList.remove('hidden');
+       LinkID = e.currentTarget.dataset.linkId;
+       updateMenuContent(LinkID)
+        
+    })
+    linkHead.addEventListener('mouseleave', () =>{
+        linkHead.classList.remove('links-toggle')
+        document.querySelector('.example').classList.add('hidden');
+    })
+})
+function updateMenuContent(LinkID){
+    
 let linksMenuHtml = '';
+let matching;
 menu.forEach((menuItem) =>{
-    linksMenuHtml += `
+    const menucategory = menuItem.category;
+    console.log(menucategory);
+    
+    if(LinkID.trim() === menucategory.trim()){
+        
+        matching = menuItem;
+        linksMenuHtml += `
     <div class="flex flex-col gap-5 items-center">
                     <div class="w-44">
-                        <img src="${menuItem.image}" alt="" class="w-full">
+                        <img src="${matching.image}" alt="" class="w-full">
                     </div>
                     <div class="text-center">
-                        <div><p>${menuItem.name}</p></div>
+                        <div><p>${matching.name}</p></div>
                         <div class="flex gap-3 underline text-gray-400">
                             <a href="">Learn</a>
                             <a href="">Order</a>
@@ -17,17 +44,9 @@ menu.forEach((menuItem) =>{
                     </div>
                 </div>
     `;
-
+    }
 });
 linkMenu.innerHTML = linksMenuHtml;
 
-document.querySelectorAll('.links').forEach((link)=>{
-    link.addEventListener('mouseover', () =>{
-        linkHead.classList.add('links-toggle');
-       document.querySelector('.example').classList.remove('hidden');
-    })
-    linkHead.addEventListener('mouseleave', () =>{
-        linkHead.classList.remove('links-toggle')
-        document.querySelector('.example').classList.add('hidden');
-    })
-})
+
+}
--- js/menu.js
+++ js/menu.js
@@ -3,36 +3,54 @@ export const menu = [
     id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     name:"Model X",
     image:"/build/images/menu/Mega-Menu-Vehicles-Model-X.png",
-    category:"vehicles"
+    category:"Vehicles"
    },
    {
-    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
     name:"Model 3",
     image:"/build/images/menu/Mega-Menu-Vehicles-Model-3-Performance-LHD.png",
-    category:"vehicles"
+    category:"Vehicles"
    },
    {
-    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    id:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     name:"Model S",
     image:"/build/images/menu/Mega-Menu-Vehicles-Model-S.png",
     category:"vehicles"
    },
    {
-    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    id:"54e0eccd-8f36-462b-b68a-8182611d9add",
     name:"Model Y",
     image:"/build/images/menu/Mega-Menu-Vehicles-Model-Y.png",
-    category:"vehicles"
+    category:"Vehicles"
    },
    {
-    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    id:"3ebe75dc-64d2-4137-8860-1f5a963e534b",
     name:"Cybertruck",
     image:"/build/images/menu/Mega-Menu-Vehicles-Cybertruck-1x.png",
-    category:"vehicles"
+    category:"Vehicles"
    },
    {
-    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    id:"8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     name:"Help Me Choose",
     image:"/build/images/menu/Mega-Menu-Vehicles-HMC-RedBlue-LHD.png",
-    category:"vehicles"
+    category:"Vehicles"
+   },
+   {
+    id:"dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    name:"Help Me Choose",
+    image:"/build/images/menu/Mega-Menu-Vehicles-HMC-RedBlue-LHD.png",
+    category:"Energy"
+   },
+   {
+    id:"dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    name:"Help Me Choose",
+    image:"/build/images/menu/Mega-Menu-Vehicles-HMC-RedBlue-LHD.png",
+    category:"Energy"
+   },
+   {
+    id:"dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    name:"Help Me Choose",
+    image:"/build/images/menu/Mega-Menu-Vehicles-HMC-RedBlue-LHD.png",
+    category:"Energy"
    },
 ]
