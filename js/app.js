5f6d6436b3c5f7701ad66477885785254c38d2f3
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-05-25 13:38:59 +0100

    added link menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
+import { menu } from "./menu.js";
 const linkHead = document.querySelector('.link-head');
+const linkMenu = document.querySelector('.links-menu');
+let linksMenuHtml = '';
+menu.forEach((menuItem) =>{
+    linksMenuHtml += `
+    <div class="flex flex-col items-center">
+                    <div class="w-44">
+                        <img src="${menuItem.image}" alt="" class="w-full">
+                    </div>
+                    <div class="text-center">
+                        <div><p>${menuItem.name}</p></div>
+                        <div class="flex gap-3 underline text-gray-400">
+                            <a href="">Learn</a>
+                            <a href="">Order</a>
+                        </div>
+                    </div>
+                </div>
+    `;
+
+});
+linkMenu.innerHTML = linksMenuHtml;
+
 document.querySelectorAll('.links').forEach((link)=>{
     link.addEventListener('mouseover', () =>{
         linkHead.classList.add('links-toggle');
